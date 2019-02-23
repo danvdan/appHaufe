ca158cd7b81e3df639056b1bd9079f2ec5614d09
dvidoni <vidonidan@gmail.com>
2019-02-23 23:54:00 +0200

    backend now running and connection established

--- back/app.js
+++ back/app.js
@@ -3,11 +3,24 @@ const Mongoose = require("mongoose");
 
 const server = new Hapi.server({
   port: 3000,
-  host: "localhost"
+  host: "0.0.0.0"
 });
 
-// Mongoose.connect("mongodb://mongo:27017/");
+let isConnected = false;
 
+Mongoose.connect("mongodb://mongo:27017/orders");
+var db = Mongoose.connection;
+
+db.on("connected", () => {
+  console.log("connected");
+  isConnected = true;
+});
+db.on("disconnected", () => {
+  console.log("disconnected");
+  isConnected = false;
+});
+
+//TODO remove
 server.route({
   method: "GET",
   path: "/",
@@ -18,9 +31,17 @@ server.route({
 
 server.route({
   method: "GET",
-  path: "/heartbeat",
+  path: "/connected",
   handler: (request, h) => {
-    return "Connection is alive!";
+    return isConnected;
+  }
+});
+
+server.route({
+  method: "GET",
+  path: "/aggregation-criteria",
+  handler: (request, h) => {
+    return "Map Reduce";
   }
 });
 
